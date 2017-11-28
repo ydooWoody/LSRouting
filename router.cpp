b7509e143d698bc4cc34f12dc1158bef8f03ad92
ydooWoody <ydoowoody@live.com>
2017-11-27 19:09:49 -0700

    TCP Should be all but done, log files being written
    At this point:
    -The manager parses the file into 2 different object vectors, one for holding connections (Links) and one for holding packets (srcdest).
    -The manager takes the parsed information and spawns X threads and builds a router object for each.
    -The manager creates sockets for each thread and begins listening
    -The manager sets up a select() call through acceptAny() function to accept incoming connections
    -While this is happening, each router is setting up both a UDP socket and a TCP socket
    -Each router tries to connect to the manager
    -Once a connection is established, the connections are stored in a vector by the manager
    -The manager then uses a function call to buildNeighbors() to find all Links that mention a given router and packages them into a msg.
    -This message is then sent through the associated connections.
    -The manager waits for ACKs from all routers before proceeding.
    -Once a router sends off an ACK at this point, they begin waiting for a $Start message.
    -The manager uses the connection vector once again to send off all $Start messages.
    -This part is where we will implement the table creation and the algorithm.
    -From here in manager and router, I put a basic framework for how to proceed after the fact, any of this can change.

--- router.cpp
+++ router.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <cstring>
 #include <stdio.h>
 #include <sys/types.h>
@@ -16,24 +17,171 @@
 
 using namespace std;
 
-Router::Router(vector<Link> neighbors, int id){
-	filename = "Router" + to_string(id) + ".txt";
+Router::Router(int id) {
+	filename = "Router" + to_string(id) + ".log";
 	file.open(filename);
-	file << "Router " << id << "\nNeighbor list size: " << neighbors.size() << "\n";
 }
-int Router::test(){
-	cout << "TESTTTTTTTTT" << endl;
+int Router::run(int port) {
+	file << "Router starting up!" << endl;
+	UDPPort = port;
+	createUDPSocket(UDPPort);
+	createTCPSocket(TCPIP, to_string(TCPPort));
 	return 1;
 }
+
+void Router::setIP(string ip) {
+	TCPIP = ip;
+}
+void Router::setPort(int port) {
+	TCPPort = port;
+}
+vector<Link> Router::messageToLinks(string message) {
+	vector < Link > neighbors;
+	string trucMessage = message.substr(1, message.length() - 1);
+	replace(trucMessage.begin(), trucMessage.end(), ':', ' ');
+	vector<string> links;
+	stringstream ss(trucMessage);
+	string temp;
+	while (ss >> temp) {
+		links.push_back(temp);
+	}
+	for (size_t i = 0; i < links.size(); i++) {
+		replace(links[i].begin(), links[i].end(), ',', ' ');
+		vector<string> vals;
+		stringstream ss1(links[i]);
+		string temp1;
+		while (ss1 >> temp1) {
+			vals.push_back(temp1);
+		}
+		neighbors.push_back(Link(atoi(vals[0].c_str()), atoi(vals[1].c_str()), atoi(vals[2].c_str())));
+	}
+	return neighbors;
+}
+//Send to a TCP connection
+void Router::sendTCP(int fd, string message) {
+	//Sending
+	char buffer[9999];
+
+	//Writing a message to buffer + handling for too many characters.
+	memset(&buffer, 0, sizeof(buffer));
+	strcpy(buffer, message.c_str());
+
+	//Send message
+	send(fd, (char*) &buffer, strlen(buffer), 0);
+	file << "Message sent." << endl;
+}
+
+//Send back to the addr that sent to you
+void sendBack(int fd, struct sockaddr_in addr, string message) {
+	char msg[9999];
+
+	//Writing a message to buffer
+	memset(&msg, 0, sizeof(msg));
+	strcpy(msg, message.c_str());
+
+	sendto(fd, msg, strlen(msg), 0, (struct sockaddr *) &addr, sizeof(addr));
+}
+
+//Try receiving from this TCP connection.
+string Router::receiveTCP(int fd) {
+	char buffer[9999];
+	memset(&buffer, 0, sizeof(buffer));
+	//receive file size
+	file << "READY! Waiting for message.\n" << endl;
+	while (strlen(buffer) == 0) {
+		recv(fd, (char*) &buffer, sizeof(buffer), 0);
+	}
+	file << "Received a message." << endl;
+	string str(buffer);
+	file << str << endl;
+	return str;
+}
+
+//Create a manager side socket for connecting to routers.
+int Router::createTCPSocket(string destIp, string destPort) {
+	file << "\n-----TCP STARTUP-----" << endl;
+	file << "Trying to create TCP Socket" << endl;
+
+	//Variables for socket file descriptor and address info
+	int fd;
+	struct addrinfo hints, *res;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	const char* ipChar = destIp.c_str();
+	const char* portChar = destPort.c_str();
+	getaddrinfo(ipChar, portChar, &hints, &res);
+
+	//Create a socket
+	fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	//Avoid "address already in use" error because machine(?) still waiting to send ack and keeps getting sent into TIMEWAIT.
+	int optval = 1;
+	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
+	if (fd < 0) {
+		char const *p = "ERROR creating socket";
+		error(p);
+	}
+	file << "TCP Socket Created!" << endl;
+	//Connect to server.
+	int conErr = connect(fd, res->ai_addr, res->ai_addrlen);
+	if (conErr < 0) {
+		char const *p = "ERROR creating connection";
+		error(p);
+	}
+
+	file << "Connection established with server. Port: " << destPort << "\n" << endl;
+	nodeNum = (atoi(destPort.c_str()) - 7000);
+	bool finished = false;
+	while (!finished) {
+		file << "Waiting" << endl;
+		string message = receiveTCP(fd);
+		if (message.at(0) == '*') {
+			if (message != "*None") {
+				neighbors = messageToLinks(message);
+				file << "-----RECEIVED LINKS OVER TCP-----" << endl;
+				for (size_t i = 0; i < neighbors.size(); i++) {
+					file << "SRC: " << neighbors[i].src << " Dest: " << neighbors[i].dest << " Cost: " << neighbors[i].cost << endl;
+				}
+			}
+			file << "This router is node: " << nodeNum << endl;
+			struct sockaddr_in sin;
+			file << "Sending ACK back to manager." << endl;
+			sendBack(fd, sin, "ACK");
+			file << "ACK sent to manager." << endl;
+			file << "\nAwaiting START signal...\n" << endl;
+		} else if (message.at(0) == '$') {
+			struct sockaddr_in sin;
+			file << "Received start" << endl;
+			//... THIS IS WHERE A CALL TO START LS ROUTING BEGINS
+			file << "Finished my table." << endl;
+			sendBack(fd, sin, "DONE");
+			file << "\nAwaiting PACKET signal...\n" << endl;
+		} else if (message.at(0) == '#') {
+			struct sockaddr_in sin;
+			file << "Received a packet." << endl;
+			//...THIS SHOULD BE A CALL TO SEND TO ANOTHER ROUTER OVER UDP OR THE MANAGER OVER TCP
+			file << "Sending it off to another router or sending a received to manager." << endl;
+			sendBack(fd, sin, "RECV");
+		} else if(message.at(0) == '!'){
+			exit(1);
+		} else {
+			close(fd);
+		}
+	}
+	return fd;
+}
+
 /**
  * These methods are for UDP connections.
  * There is one method for creating the socket for sending/receiving
  * There is a method for sending works via FD and a destination IP
  * Receive method listens on the default port for messages
  **/
-int PORT = 6789;
+
 //Create a routerA side socket for connecting to routers.
-int createUDPSocket() {
+int Router::createUDPSocket(int port) {
+	file << "\n-----UDP STARTUP-----" << endl;
+	file << "Trying to create UDP Socket" << endl;
 	int fd;
 	struct sockaddr_in myaddr;
 
@@ -48,12 +196,12 @@ int createUDPSocket() {
 	gethostname(hostname, sizeof hostname);
 	he = gethostbyname(hostname);
 	addr_list = (struct in_addr **) he->h_addr_list;
-	cout << "Made socket: <" << inet_ntoa(*addr_list[0]) << ", " << PORT << ">" << endl;
+	file << "UDP socket created: <" << inet_ntoa(*addr_list[0]) << ", " << port << ">" << endl;
 
 	memset((char *) &myaddr, 0, sizeof(myaddr));
 	myaddr.sin_family = AF_INET;
 	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	myaddr.sin_port = htons(PORT);
+	myaddr.sin_port = htons(port);
 
 	if (bind(fd, (struct sockaddr *) &myaddr, sizeof(myaddr)) < 0) {
 		char const *p = "ERROR binding socket";
@@ -63,7 +211,7 @@ int createUDPSocket() {
 }
 
 //Send to a UDP connection over a fd
-void sendUDP(int fd, string destIp, string message) {
+void Router::sendUDP(int fd, string destIp, string message) {
 	struct sockaddr_in servaddr;
 	char msg[9999];
 
@@ -74,7 +222,7 @@ void sendUDP(int fd, string destIp, string message) {
 	//Set up servaddr
 	memset((char*) &servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(PORT);
+	servaddr.sin_port = htons(UDPPort);
 
 	//Copy the destIP to the servaddr stuct
 	inet_pton(AF_INET, destIp.c_str(), &(servaddr.sin_addr));
@@ -85,19 +233,7 @@ void sendUDP(int fd, string destIp, string message) {
 		error(p);
 	}
 
-	cout << "Message sent!" << endl;
-}
-
-//Send back to the addr that sent to you
-void sendBack(int fd, struct sockaddr_in addr, string message) {
-	char msg[9999];
-
-	//Writing a message to buffer
-	memset(&msg, 0, sizeof(msg));
-	strcpy(msg, message.c_str());
-
-	sendto(fd, msg, strlen(msg), 0, (struct sockaddr *) &addr, sizeof(addr));
-	cout << "Sending message back to sender!" << endl;
+	file << "Message sent!" << endl;
 }
 
 //Start receiving UDP messages, complete with ACK handling
